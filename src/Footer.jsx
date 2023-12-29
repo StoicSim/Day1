460ce30ccf5ba8113a910f1f14f990f27802e7e7
Simran Maharjan <078bct089.simran@pcampus.edu.np>
2023-12-29 23:16:26 +0545

    Day_1_assignment

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Footer = (props) => {
+    return (
+        <div>
+            <h2>{props.title}</h2>
+
+
+
+
+        </div>
+    );
+};
+export default Footer;
---
+++ src/SideNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const SideNav = (props) => {
+    return (
+        <div>
+            <h2>{props.title}</h2>
+
+
+
+
+        </div>
+    );
+};
+export default SideNav;
